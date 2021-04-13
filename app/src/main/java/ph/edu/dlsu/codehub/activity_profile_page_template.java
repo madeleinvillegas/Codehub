018bd777c807a02c85eb0e6346cb745a5a382129
eugene reyes <eugenecedricngoreyes@gmail.com>
2021-04-14 00:21:39 +0800

    "working xml" for profile page template. will still need to add the my profile page fragment. delayed because youtube video watched and read is different from what the code already implemented. will probably finish by the weekend sorry.

--- app/src/main/java/ph/edu/dlsu/codehub/activity_profile_page_template.java
+++ app/src/main/java/ph/edu/dlsu/codehub/activity_profile_page_template.java
@@ -6,7 +6,6 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,11 +13,6 @@ import android.view.Menu;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class activity_profile_page_template extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-
-    private SectionsStatePagerAdapter mSectionsStatePagerAdapter;
-    private ViewPager mViewPager;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +31,6 @@ public class activity_profile_page_template extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
-        //fragment related stuff
-        mSectionsStatePagerAdapter = new SectionsStatePagerAdapter (getSupportFragmentManager());
-        mViewPager = (ViewPager) findViewById(R.id.fragment_body);
-        //setup the pager
-
-        setupViewPager(mViewPager);
     }
 
     @Override
@@ -51,18 +38,4 @@ public class activity_profile_page_template extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.top_bar, menu);
         return true;
     }
-    public void setViewPager(int fragmentNumber){
-        mViewPager.setCurrentItem(fragmentNumber);
-    }
-
-    private void setupViewPager(ViewPager viewPager)
-    {
-        SectionsStatePagerAdapter adapter = new SectionsStatePagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new fragment_create_post(), "Create Post Fragment");
-        adapter.addFragment(new fragment_my_profile(), "My Profile Fragment");
-        adapter.addFragment(new fragment_notification(), "Notification Fragment");
-        adapter.addFragment(new fragment_home(), "Home Fragment");
-        viewPager.setAdapter(adapter);
-    }
-
 }

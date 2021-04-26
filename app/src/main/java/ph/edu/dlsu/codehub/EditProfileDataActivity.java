88c4175b7887f583236efa10c1776f2d736324b8
Euge908 <eugene_cedric_reyes@dlsu.edu.ph>
2021-04-26 12:16:56 +0800

    Fixed bug regarding constraint layout and scroll view. For some reason the on click lisener for the save changes button seems to crash the program. I will look into it.

--- app/src/main/java/ph/edu/dlsu/codehub/EditProfileDataActivity.java
+++ app/src/main/java/ph/edu/dlsu/codehub/EditProfileDataActivity.java
@@ -1,17 +1,23 @@
 package ph.edu.dlsu.codehub;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -31,21 +37,25 @@ public class EditProfileDataActivity extends AppCompatActivity {
     private ImageView currentBackgroundPicture;
     private CircleImageView currentProfilePicture;
 
-
     private FirebaseAuth mAuth;
     private DatabaseReference usersDatabaseReference;
     private String currentUserId;
 
+    private ProgressBar progressBar;
+
+    private String TAG = "DEBUGGING TAG";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Log.d(TAG, "Initializing Firebase Current User");
         mAuth = FirebaseAuth.getInstance();
         currentUserId = mAuth.getCurrentUser().getUid();
         usersDatabaseReference = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId);
+        Log.d(TAG, "Finished Firebase Current User");
 
 
-
+        Log.d(TAG, "Started to Get Android Id's");
         //ID references
         editProfilePicture = (TextView) findViewById(R.id.edit_profile_picture);
         editBackgroundPicture = (TextView) findViewById(R.id.edit_background_image);
@@ -57,20 +67,40 @@ public class EditProfileDataActivity extends AppCompatActivity {
         saveChanges = (Button) findViewById(R.id.save_changes_button);
         currentProfilePicture = (CircleImageView) findViewById(R.id.profile_picture) ;
         currentBackgroundPicture = (ImageView) findViewById(R.id.background_image);
+        Log.d(TAG, "Successfully Gotten The Android Id's");
 
+        Log.d(TAG, "Starting relative layout with progressbar");
+//        RelativeLayout layout = new RelativeLayout(this);
+//        progressBar = new ProgressBar(EditProfileDataActivity.this,null,android.R.attr.progressBarStyleLarge);
+//        progressBar.setIndeterminate(true);
+//        progressBar.setVisibility(View.GONE);
 
+//        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(100,100);
+//        params.addRule(RelativeLayout.CENTER_IN_PARENT);
+//        layout.addView(progressBar,params);
+//        setContentView(layout);
+        Log.d(TAG, "Successfully finished relative layout with progressbar");
 
+        Log.d(TAG, "Adding on click listener for button");
+//        saveChanges.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Log.d(TAG, "INSIDE THE ON CLICK LISTENER");
+//                saveAccountInformation();
+//            }
+//        });
+        Log.d(TAG, "Successfully Added on click listener for button");
+        Log.d(TAG, "Adding Layout");
 
-        saveChanges.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                saveAccountInformation();
-            }
-        });
         setContentView(R.layout.activity_edit_profile);
+        Log.d(TAG, "Ended Layout");
+
+
     }
 
     private void saveAccountInformation(){
+        Log.d(TAG, "Calling Save Account Information");
+
         String profilePictureText, backgroundPictureText, firstNameText, lastNameText, currentUserNameText, currentAddressText, currentOccupationText;
 
         //assumption is that every field is mandatory
@@ -106,6 +136,8 @@ public class EditProfileDataActivity extends AppCompatActivity {
         }
         else
         {
+            progressBar.setVisibility(View.VISIBLE);
+
             HashMap userMap = new HashMap();
             userMap.put("username", currentUserNameText);
             userMap.put("firstName", firstNameText);
@@ -126,16 +158,23 @@ public class EditProfileDataActivity extends AppCompatActivity {
                     {
                         sendUserToHomePage();  // send user to main activity
                         Toast.makeText(EditProfileDataActivity.this, "Profile Data Changed Successfully", Toast.LENGTH_LONG);
+                        progressBar.setVisibility(View.GONE);
+
                     }
                     else
                     {
                         String errorMessage = task.getException().getMessage();
                         Toast.makeText(EditProfileDataActivity.this, errorMessage, Toast.LENGTH_LONG);
+                        progressBar.setVisibility(View.GONE);
+
 
                     }
                 }
             });
         }
+
+        Log.d(TAG, "Finished calling save account information");
+
     }
 
     private void sendUserToHomePage() {
